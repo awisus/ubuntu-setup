55dc12eb228eedfd8525fbc7bf3f2084348645ba
Jens Awisus <awisus.jens@gmail.com>
2021-04-26 08:52:38 +0000

    simplify docker installation

--- tools/docker.sh
+++ tools/docker.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 
-sudo rm -rf /etc/apt/sources.list.d/docker.list
-sudo rm -rf $HOME/.local/bin/docker-compose
-
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee -a /etc/apt/sources.list.d/docker.list
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-
-sudo apt-get update
 sudo apt-get install -y \
-	docker-ce  \
-	docker-ce-cli  \
-	containerd.io
-
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+     docker.io \
+     docker-compose
 
 sudo groupadd docker
 sudo usermod -aG docker $USER
