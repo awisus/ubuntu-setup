0452d58855297929953178fea867fd31c43e8c68
Jens Awisus <awisus.jens@gmail.com>
2021-06-21 14:14:22 +0000

    insomnia repo

--- apps/insomnia.sh
+++ apps/insomnia.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo rm -rf /etc/apt/sources.list.d/insomnia.list
-echo "deb https://dl.bintray.com/getinsomnia/Insomnia /" | sudo tee -a /etc/apt/sources.list.d/insomnia.list
+echo "deb [trusted=yes arch=amd64] https://download.konghq.com/insomnia-ubuntu/ default all" | sudo tee -a /etc/apt/sources.list.d/insomnia.list
 wget -O- https://insomnia.rest/keys/debian-public.key.asc | sudo apt-key add -
 
 sudo apt update
