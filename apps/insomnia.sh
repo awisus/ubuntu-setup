8a3c54946cd32f6d23c7d004b038eda5bcc825f3
Jens Awisus <awisus.jens@gmail.com>
2021-10-23 14:54:29 +0000

    insomnia installation

--- apps/insomnia.sh
+++ apps/insomnia.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo rm -rf /etc/apt/sources.list.d/insomnia.list
-echo "deb [trusted=yes arch=amd64] https://download.konghq.com/insomnia-ubuntu/ default all" | sudo tee -a /etc/apt/sources.list.d/insomnia.list
-wget -O- https://insomnia.rest/keys/debian-public.key.asc | sudo apt-key add -
+PACKAGE=/tmp/insomnia.deb
+wget "https://updates.insomnia.rest/downloads/ubuntu/latest?=&app=com.insomnia.app" -O $PACKAGE
 
-sudo apt update
-sudo apt install -y insomnia
+sudo dpkg -i $PACKAGE
+
+rm $PACKAGE
