648b030aa1081e825c9b9c2bb047ddc2e6666385
Jens Awisus <awisus.jens@gmail.com>
2022-04-25 19:37:39 +0000

    firefox deb on ubuntu 22.04

--- install.sh
+++ install.sh
@@ -3,6 +3,14 @@
 # apps
 sudo apt update
 sudo apt dist-upgrade -y
+sudo add-apt-repository ppa:mozillateam/ppa -y
+
+echo '
+Package: *
+Pin: release o=LP-PPA-mozillateam
+Pin-Priority: 1001
+' | sudo tee /etc/apt/preferences.d/mozilla-firefox
+echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
 
 sudo apt install -y \
      build-essential \
