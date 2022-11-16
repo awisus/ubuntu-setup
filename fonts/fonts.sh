432153fac37a7eff9eb7719dc9f5869b6fbcd990
Jens Awisus <awisus.jens@gmail.com>
2022-11-16 20:08:40 +0100

    firacode nerd font

--- fonts/fonts.sh
+++ fonts/fonts.sh
@@ -6,10 +6,13 @@ sudo cp -r \
     fonts/fira \
     /usr/share/fonts/opentype
 sudo cp -r \
+    fonts/fira-code-nerd-font \
+    /usr/share/fonts/truetype
+sudo cp -r \
     fonts/roboto-slab \
     /usr/share/fonts/truetype
 
 gsettings set org.gnome.desktop.interface font-name 'Fira Sans Book 10'
 gsettings set org.gnome.desktop.interface document-font-name 'Roboto Slab Regular 11'
-gsettings set org.gnome.desktop.interface monospace-font-name 'Fira Code Retina 11'
+gsettings set org.gnome.desktop.interface monospace-font-name 'FiraCode Nerd Font weight=453 11'
 gsettings set org.gnome.desktop.wm.preferences titlebar-font 'Fira Sans SemiBold 10'
