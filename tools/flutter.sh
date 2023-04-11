2860d5d363867dc54ad2a314fdd9492aa50fe6a0
Jens Awisus <awisus@users.noreply.github.com>
2023-04-11 13:17:41 +0200

    add flutter desktop dependencies

--- tools/flutter.sh
+++ tools/flutter.sh
@@ -5,3 +5,6 @@ rm -rf $HOME/flutter
 git clone https://github.com/flutter/flutter.git -b stable $HOME/flutter
 
 $HOME/flutter/bin/flutter precache
+$HOME/flutter/bin/flutter config --enable-linux-desktop
+
+sudo apt-get install clang cmake ninja-build pkg-config libgtk-3-dev liblzma-dev libstdc++-12-dev
